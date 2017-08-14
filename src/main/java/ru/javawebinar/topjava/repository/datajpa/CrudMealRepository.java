39d8f04348723c8b6f50c8cda109cf6850eabab8
Kowi77 <kovalenka@mail.ru>
2017-08-14 16:02:34 +0700

    HW5 ver 1.1

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,30 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.util.List;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    @Override
+    List<Meal> findAll(Sort sort);
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int delete(@Param("id") int id, @Param("userId") int userId);
+
+    @Override
+    Meal findOne(Integer integer);
+
+
 }
