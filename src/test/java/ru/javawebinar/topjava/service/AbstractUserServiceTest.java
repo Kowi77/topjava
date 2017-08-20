31ad1278179bdbea6ea09fdec43c0fc717bda15d
Kowi77 <kovalenka@mail.ru>
2017-08-20 10:38:19 +0700

    6 05 HW5 optional fetch join

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -15,7 +15,7 @@ import static ru.javawebinar.topjava.UserTestData.*;
 public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Autowired
-    private UserService service;
+    protected UserService service;
 
     @Before
     public void setUp() throws Exception {
